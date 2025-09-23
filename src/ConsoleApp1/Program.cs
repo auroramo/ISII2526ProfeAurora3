e34c9308f32b69a6d60eb2c5bfc44bdf16671733
AURORA MACIAS OJEDA <Aurora.Macias@alu.uclm.es>
2025-09-23 20:15:44 +0200

    Añadido mensaje evitar conflicto

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de verduras con limón y mejillones!");
+Console.WriteLine("Para evitar conflictos...");
