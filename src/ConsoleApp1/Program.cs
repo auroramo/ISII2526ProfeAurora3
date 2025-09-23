b1cda13395551a7dfc54f963b22a947afd0e65e9
AURORA MACIAS OJEDA <Aurora.Macias@alu.uclm.es>
2025-09-23 19:59:02 +0200

    Añadido ingredientes

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella!");
+Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de verduras!");
