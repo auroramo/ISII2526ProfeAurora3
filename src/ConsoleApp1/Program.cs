14ff8bf5697457ae018ac545870f4bbf65d1ab72
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-09-23 20:09:43 +0200

    Update Program.cs

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de verduras con limón!");
+Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de verduras con limón y mejillones!");
