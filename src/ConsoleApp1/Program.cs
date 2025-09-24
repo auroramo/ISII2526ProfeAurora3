7b4c50d31e61b539a4dfe1c99e99e6e0d0a423fd
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-09-24 19:10:15 +0200

    Update Program.cs 3 - Cocinar

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de verduras con limón, mejillones y calamares!");
+
+int atributo1 = 1;
+
+void cocinar () {
+  Console.WriteLine("Cocinando paella...");
+}
