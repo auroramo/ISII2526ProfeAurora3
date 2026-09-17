6efe784418b8022a9e30c8e337c535e58ca88b7c
auroramo <Aurora.Macias@alu.uclm.es>
2026-09-17 12:46:43 +0100

    Nueva version donetcore y nueva funcion prueba (modificada) en program.cs

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -3,6 +3,11 @@ Console.WriteLine("Hello, World! I come from Spain. ¡Qué rica la paella de ver
 
 int atributo1 = 1;
 
+void Prueba()
+{
+    Console.WriteLine("Hola, mundo");
+}
+
 void Cocinar ()
 {
     Console.WriteLine("Cocinando paella...");
@@ -12,3 +17,5 @@ void Comer ()
 {
     Console.WriteLine("Comiendo paella...");
 }
+
+
